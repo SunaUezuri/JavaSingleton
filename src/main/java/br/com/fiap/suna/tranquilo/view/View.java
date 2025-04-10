d85af5d4c5efd7cd2943d74698d35ff417bfb28f
SunaUezuri <wesley.jane.santos@gmail.com>
2025-04-10 19:55:07 -0300

    Refazendo a View para os testes

--- src/main/java/br/com/fiap/suna/tranquilo/view/View.java
+++ src/main/java/br/com/fiap/suna/tranquilo/view/View.java
@@ -1,7 +1,56 @@
 package br.com.fiap.suna.tranquilo.view;
 
+import br.com.fiap.suna.tranquilo.dao.InvestimentoDao;
+import br.com.fiap.suna.tranquilo.dao.InvestimentoDaoImpl;
+import br.com.fiap.suna.tranquilo.entity.Investimento;
+import br.com.fiap.suna.tranquilo.entity.TipoInvestimento;
+import br.com.fiap.suna.tranquilo.exception.CommitException;
+import br.com.fiap.suna.tranquilo.exception.IdNaoEncontradoException;
+import br.com.fiap.suna.tranquilo.singleton.EntityManagerFactorySingleton;
+import javax.persistence.EntityManager;
+
 public class View {
     public static void main(String[] args) {
+        EntityManager em = EntityManagerFactorySingleton.getInstance().createEntityManager();
 
+        InvestimentoDao dao = new InvestimentoDaoImpl(em);
+
+        Investimento investimento = new Investimento("Tranquilo Coin Better", 10000001.0, TipoInvestimento.CRIPTO);
+
+        //Cadastrar
+        try {
+            dao.salvar(investimento);
+            dao.commit();
+            System.out.println("Investimento Cadastrado!");
+        } catch (CommitException e){
+            System.out.println(e.getMessage());
+        }
+
+        //Read
+        try {
+            Investimento busca = dao.buscar(2);
+            System.out.println(busca.getNome());
+
+            //Atualizar
+            busca.setValor(1000.0);
+            dao.salvar(busca);
+            dao.commit();
+            System.out.println("Investimento atualizado");
+        } catch (IdNaoEncontradoException e) {
+            System.out.println(e.getMessage());
+        } catch (CommitException f) {
+            System.out.println(f.getMessage());
+        }
+
+        //Deletar
+        try {
+            dao.remover(2);
+            dao.commit();
+            System.out.println("Investimento removido!");
+        } catch (CommitException e) {
+            System.out.println(e.getMessage());
+        } catch (IdNaoEncontradoException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
